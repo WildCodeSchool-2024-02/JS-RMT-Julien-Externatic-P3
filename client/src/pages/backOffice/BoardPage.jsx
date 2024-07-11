0cfc3f138d74090bfdc8dceea799f4038b4294e6
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-07-11 17:08:18 +0200

    Création de la page du board de l'admin avec sont fichiers css et du composant associé

---
+++ client/src/pages/backOffice/BoardPage.jsx
@@ -0,0 +1,13 @@
+import Board from "../../components/backOffice/boardComponent/Board";
+
+import "./BoardPage.css";
+
+function BoardPage() {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+}
+
+export default BoardPage;
