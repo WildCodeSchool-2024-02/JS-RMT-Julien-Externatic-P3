97b5f238aa2550a542ad2f6a7e1f53bb01c26b7a
BastienDmr <domer.bastien@gmail.com>
2024-07-11 17:08:01 +0200

    Init page et composant details

---
+++ client/src/components/backOffice/detailsComponent/Details.jsx
@@ -0,0 +1,7 @@
+import "./Details.css";
+
+function Details() {
+  return <div>details composent</div>;
+}
+
+export default Details;
---
+++ client/src/pages/backOffice/DetailsPage.jsx
@@ -0,0 +1,13 @@
+import Details from "../../components/backOffice/detailsComponent/Details";
+import "./DetailsPage.css";
+
+function DetailsPage() {
+  return (
+    <section>
+      <div>details page</div>
+      <Details />
+    </section>
+  );
+}
+
+export default DetailsPage;
