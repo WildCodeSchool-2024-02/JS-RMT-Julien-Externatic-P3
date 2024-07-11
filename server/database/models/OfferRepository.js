269a0f4a36bdc6cf7fd1f32343cf7469df0763f5
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-07-11 10:47:57 +0200

    Backend Browse Offer

---
+++ server/database/models/OfferRepository.js
@@ -0,0 +1,16 @@
+const AbstractRepository = require("./AbstractRepository");
+
+class OfferRepository extends AbstractRepository {
+  constructor() {
+    // Call the constructor of the parent class (AbstractRepository)
+    // and pass the table name "item" as configuration
+    super({ table: "offer" });
+  }
+
+  async readAll() {
+    const [rows] = await this.database.query(`select * from ${this.table}`);
+    return rows;
+  }
+}
+
+module.exports = OfferRepository;
