ba5571eaef7dcadf4180cd0180e25f971fe8f211
BastienDmr <domer.bastien@gmail.com>
2024-07-11 17:04:31 +0200

    Recuperation de mes données dans le main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,23 @@ import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import connexion from "./services/connexion";
+
 import App from "./App";
+import DetailsPage from "./pages/backOffice/DetailsPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/admin/entreprises/:id",
+    element: <DetailsPage />,
+    loader: async ({ params }) => {
+      const response = await connexion.get(`/api/companies/${params.id}`);
+      return response.data;
+    },
   },
 ]);
 
