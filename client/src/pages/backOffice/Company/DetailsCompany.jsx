eadf303d499241ca2c34476b0b643e3dd637a80d
BastienDmr <domer.bastien@gmail.com>
2024-07-12 12:35:26 +0200

    Initialisation de la page détail d'une entreprise

--- client/src/pages/backOffice/Company/DetailsCompany.jsx
+++ client/src/pages/backOffice/Company/DetailsCompany.jsx
@@ -2,14 +2,28 @@ import { useLoaderData } from "react-router-dom";
 import Details from "../../../components/backOffice/detailsComponent/Details";
 import "./DetailsCompany.css";
 
-function DetailsPage() {
-  const companyDetails = useLoaderData;
+function DetailsCompany() {
+  const companyDetails = useLoaderData();
+  const subtitle = [
+    "Siege social :",
+    "Nombres d'employés :",
+    "Chiffres d'affaires :",
+    "€/ Annuel",
+    "Secteur d'activité :",
+    "Valeurs :",
+    "Description :",
+    "Nom du consultant :",
+    "Nom du contact :",
+    "Téléphone du Contact :",
+    "Mail du contact :",
+  ];
   return (
-    <section>
-      <div>details page</div>
-      <Details company={companyDetails} />
-    </section>
+    <Details
+      data={companyDetails}
+      subtitle={subtitle}
+      className="container-info-company"
+    />
   );
 }
 
-export default DetailsPage;
+export default DetailsCompany;
