12f57dfdd435f7d9f9167f715823ab20618681a2
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-07-12 08:48:32 +0200

    modification du schéma sql pour allonger la descriptiopn des profils

--- client/src/pages/ProfilDetails/ProfilDetails.jsx
+++ client/src/pages/ProfilDetails/ProfilDetails.jsx
@@ -1,25 +1,26 @@
 import { useLoaderData } from "react-router-dom";
 
 import avatar from "../../assets/images/avatar.png";
+import "./ProfilDetails.css";
 
 function ProfilDetails() {
   const oneProfil = useLoaderData();
 
   return (
     <>
-      <h1>Mon profil</h1>
-      <img src={avatar} alt={oneProfil.firstname} />
-      <section>
-        <article>
+      <h1 className="profil-title">Mon profil</h1>
+      <section className="profil-header">
+        <img src={avatar} alt={oneProfil.firstname} className="profil-avatar" />
+        <article className="profil-header-name-location">
           <p>{oneProfil.firstname}</p>
           <p>{oneProfil.lastname}</p>
           <p>{oneProfil.city}</p>
         </article>
-        <article>
+        <article className="profil-header-contact">
           <p>{oneProfil.mail}</p>
           <p>{oneProfil.phone}</p>
         </article>
-        <article>
+        <article className="profil-header-description">
           <p>{oneProfil.description}</p>
         </article>
       </section>
