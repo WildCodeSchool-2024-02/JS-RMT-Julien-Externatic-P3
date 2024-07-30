defe0f87df982e614759d5b39ffe43f7d1d9eb5a
BastienDmr <domer.bastien@gmail.com>
2024-07-30 16:39:37 +0200

    Finalistation de la page detail d'une entreprise

--- client/src/pages/backOffice/Company/DetailsCompany.jsx
+++ client/src/pages/backOffice/Company/DetailsCompany.jsx
@@ -1,28 +1,39 @@
 import { useLoaderData } from "react-router-dom";
-import Details from "../../../components/backOffice/detailsComponent/Details";
 import "./DetailsCompany.css";
+import H2p from "../../../components/H2p";
 
 function DetailsCompany() {
   const companyDetails = useLoaderData();
-  const subtitle = [
-    "Siege social :",
-    "Nombres d'employés :",
-    "Chiffres d'affaires :",
-    "€/ Annuel",
-    "Secteur d'activité :",
-    "Valeurs :",
-    "Description :",
-    "Nom du consultant :",
-    "Nom du contact :",
-    "Téléphone du Contact :",
-    "Mail du contact :",
-  ];
+
   return (
-    <Details
-      data={companyDetails}
-      subtitle={subtitle}
-      className="container-info-company"
-    />
+    <section className="container-info-company">
+      <h1 className="styleTitleH1">{companyDetails.name}</h1>
+      <section className="container-detail">
+        <div className="container-detail-company">
+          <H2p subtitle="Siege social :" data={companyDetails.head_office} />
+          <H2p subtitle="Nombres d'employés :" data={companyDetails.size} />
+          <H2p
+            subtitle="Chiffres d'affaires :"
+            data={companyDetails.sales_figure}
+          />
+          <H2p
+            subtitle="Secteur d'activité :"
+            data={companyDetails.activity_area_name}
+          />
+          <H2p subtitle="Valeurs :" data={companyDetails.company_values} />
+          <H2p subtitle="Description :" data={companyDetails.description} />
+        </div>
+        <div className="container-detail-contact">
+          <H2p
+            subtitle="Nom du consultant :"
+            data={companyDetails.consultant_name}
+          />
+          <H2p subtitle="Nom du contact :" data={companyDetails.contact_name} />
+          <H2p subtitle="Téléphone :" data={companyDetails.contact_phone} />
+          <H2p subtitle="Mail :" data={companyDetails.contact_mail} />
+        </div>
+      </section>
+    </section>
   );
 }
 
