abafc66774c9c39e4faa00c7e9a3ddd2f37e07fe
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-07-30 16:42:35 +0200

    rajout du dtatus 200 en cas de réussite du chargement des données

--- server/app/controllers/profilAction.js
+++ server/app/controllers/profilAction.js
@@ -26,7 +26,7 @@ const read = async (req, res, next) => {
     if (profil == null) {
       res.sendStatus(404);
     } else {
-      res.json(profil);
+      res.status(200).json(profil);
     }
   } catch (err) {
     // Pass any errors to the error-handling middleware
