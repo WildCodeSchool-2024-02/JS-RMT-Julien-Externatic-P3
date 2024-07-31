27d31262835f49339eba451e2993aef8bc8a1a98
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-07-31 15:10:05 +0200

    Création de la page board liste afin d'afficher le composant comprenant la liste des entreprises

--- client/src/pages/backOffice/Company/BoardCompanies.jsx
+++ client/src/pages/backOffice/Company/BoardCompanies.jsx
@@ -2,13 +2,11 @@ import { useLoaderData } from "react-router-dom";
 
 import BoardList from "../../../components/backOffice/boardComponent/BoardList";
 
-import "./BoardCompanies.css";
-
 function BoardCompanies() {
   const companies = useLoaderData();
   return (
     <div>
-      <BoardList company={companies} />
+      <BoardList datas={companies} />
     </div>
   );
 }
