9b67f9b67630f2713d4709b11add6c1d1e70b9d2
DylanRobinaud <164893941+DylanRobinaud@users.noreply.github.com>
2024-07-31 15:28:11 +0200

    Update router.js

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -15,12 +15,6 @@ router.use("/companies", companiesRouter);
 const offersRouter = require("./offers/router");
 
 router.use("/offers", offersRouter);
-
-
-
-const companiesRouter = require("./companies/router");
-
-router.use("/companies", companiesRouter);
 /* ************************************************************************* */
 
 module.exports = router;
