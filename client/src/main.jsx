fbc25f098836f9125b64e6f0bf61adc2dd4cd7a4
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-08-02 15:39:36 +0200

    mise en place de la route dans main et consommation des données, mise à jour de trad.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,10 +9,10 @@ import ProfilDetails from "./pages/ProfilDetails/ProfilDetails";
 import UserLayout from "./pages/ProfilDetails/UserLayout";
 import DetailsCompany from "./pages/backOffice/Company/DetailsCompany";
 import AdminLayout from "./pages/backOffice/AdminLayout/AdminLayout";
-import Offers from "./pages/Offers/Offers"
+import Offers from "./pages/Offers/Offers";
+import Consultant from "./pages/backOffice/Consultant/Consultant";
 
 import connexion from "./services/connexion";
-
 
 const router = createBrowserRouter([
   {
@@ -33,11 +33,11 @@ const router = createBrowserRouter([
       },
     ],
   },
- {
+  {
     path: "/admin/",
     element: <AdminLayout />,
     children: [
-      { 
+      {
         path: "entreprises",
         element: <BoardCompanies />,
         loader: async () => {
@@ -53,8 +53,16 @@ const router = createBrowserRouter([
           return response.data;
         },
       },
+      {
+        path: "consultants",
+        element: <Consultant />,
+        loader: async () => {
+          const response = await connexion.get("/api/users/consultants");
+          return response.data;
+        },
+      },
     ],
-  },      
+  },
   {
     path: "/offres",
     element: <Offers />,
