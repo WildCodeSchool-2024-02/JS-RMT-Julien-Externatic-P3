7e74696a135465151f9056bb785ed4fa2ac7bda5
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-08-05 17:08:28 +0200

    ajout d'une validation avant fermeture

--- client/src/components/UI/Modal.jsx
+++ client/src/components/UI/Modal.jsx
@@ -2,13 +2,24 @@ import { useState } from "react";
 import ReactModal from "react-modal";
 import PropTypes from "prop-types";
 
+import "./Modal.css";
+
 function Modal({ buttonLabel, contentLabel, Content }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [closeConfirm, setCloseConfirm] = useState(false);
   const openModal = () => {
     setIsOpen(true);
   };
   const closeModal = () => {
     setIsOpen(false);
+    setCloseConfirm(false);
+  };
+  const handleCloseRequest = () => {
+    if (closeConfirm) {
+      closeModal();
+    } else {
+      setCloseConfirm(true);
+    }
   };
 
   return (
@@ -16,10 +27,32 @@ function Modal({ buttonLabel, contentLabel, Content }) {
       <button type="button" onClick={openModal}>
         {buttonLabel}
       </button>
-      <ReactModal isOpen={isOpen} contentLabel={contentLabel}>
-        <button type="button" onClick={closeModal}>
+      <ReactModal
+        isOpen={isOpen}
+        contentLabel={contentLabel}
+        onRequestClose={handleCloseRequest}
+        shouldCloseOnEsc
+        shouldCloseOnOverlayClick
+        shouldFocusAfterRender
+      >
+        <button
+          className="close-modal"
+          type="button"
+          onClick={handleCloseRequest}
+        >
           X
         </button>
+        {closeConfirm && (
+          <div>
+            <p>Voulez-vous vraiment fermer sans sauvegarder ?</p>
+            <button type="button" onClick={closeModal}>
+              Oui
+            </button>
+            <button type="button" onClick={() => setCloseConfirm(false)}>
+              Non
+            </button>
+          </div>
+        )}
         <Content />
       </ReactModal>
     </>
