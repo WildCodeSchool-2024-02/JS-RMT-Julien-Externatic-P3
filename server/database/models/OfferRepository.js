ed3fc1f97034068af60964a4b89f6247558478b3
Bastien <164893812+BastienDmr@users.noreply.github.com>
2024-08-06 15:46:41 +0200

    Update OfferRepository.js

--- server/database/models/OfferRepository.js
+++ server/database/models/OfferRepository.js
@@ -18,6 +18,9 @@ class OfferRepository extends AbstractRepository {
       `SELECT o.id, o.title, cat.category, comp.name, o.on_updated_at, COUNT(c.candidate_id) AS candidacy_count FROM offer AS o INNER JOIN category AS cat ON cat.id = o.category_id INNER JOIN company AS comp ON comp.id = o.company_id LEFT JOIN candidacy AS c ON o.id = c.offer_id WHERE o.consultant_id = ? GROUP BY o.id, o.title, cat.category, o.start_date, o.on_updated_at`,
       [id]
     );
+    return rows;
+  }
+}
 
   async read(id) {
     const [rows] = await this.database.query(
