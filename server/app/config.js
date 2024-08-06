e76a49a53e9686b6fddf2d1ebc46fd28c9cb270c
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2024-08-06 16:54:34 +0200

    Update config.js

--- server/app/config.js
+++ server/app/config.js
@@ -1,5 +1,5 @@
 // Load the express module to create a web application
-
+const path = require("path");
 const express = require("express");
 
 const app = express();
@@ -105,9 +105,6 @@ app.use("/api", apiRouter);
 // 1. Uncomment the lines related to serving static files and redirecting unhandled requests.
 // 2. Ensure that the `reactBuildPath` points to the correct directory where your client's build artifacts are located.
 
-/*
-const path = require("path");
-
 const reactBuildPath = path.join(__dirname, "/../../client/dist");
 const publicFolderPath = path.join(__dirname, "/../public");
 
@@ -124,14 +121,12 @@ app.get("*.*", express.static(publicFolderPath, { maxAge: "1y" }));
 app.get("*", (_, res) => {
   res.sendFile(path.join(reactBuildPath, "/index.html"));
 });
-*/
 
 /* ************************************************************************* */
 
 // Middleware for Error Logging (Uncomment to enable)
 // Important: Error-handling middleware should be defined last, after other app.use() and routes calls.
 
-/*
 // Define a middleware function to log errors
 const logErrors = (err, req, res, next) => {
   // Log the error to the console for debugging purposes
@@ -144,7 +139,6 @@ const logErrors = (err, req, res, next) => {
 
 // Mount the logErrors middleware globally
 app.use(logErrors);
-*/
 
 /* ************************************************************************* */
 
