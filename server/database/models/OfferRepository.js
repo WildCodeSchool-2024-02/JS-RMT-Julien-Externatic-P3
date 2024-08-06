e5a34d8afddd1e4f122d6ea19d5a4db8fe812709
BastienDmr <domer.bastien@gmail.com>
2024-08-06 12:21:01 +0200

    Requete SQL pour venir séléctionnés les offres par consultant

--- server/database/models/OfferRepository.js
+++ server/database/models/OfferRepository.js
@@ -11,6 +11,16 @@ class OfferRepository extends AbstractRepository {
     const [rows] = await this.database.query(`select * from ${this.table}`);
     return rows;
   }
+
+  async readAllConsultantOffer(id) {
+    // Execute the SQL SELECT query to retrieve all companys from the "company" table
+    const [rows] = await this.database.query(
+      `SELECT o.id, o.title, cat.category, comp.name, o.on_updated_at, COUNT(c.candidate_id) AS candidacy_count FROM offer AS o INNER JOIN category AS cat ON cat.id = o.category_id INNER JOIN company AS comp ON comp.id = o.company_id LEFT JOIN candidacy AS c ON o.id = c.offer_id WHERE o.consultant_id = ? GROUP BY o.id, o.title, cat.category, o.start_date, o.on_updated_at`,
+      [id]
+    );
+    // Return the array of companys
+    return rows;
+  }
 }
 
 module.exports = OfferRepository;
