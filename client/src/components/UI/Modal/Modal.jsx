3053e335316a4a9a25382d707f8a3cfe1e4ee06f
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-08-07 10:58:28 +0200

    integration Modal, et creation deux types de bouttons

---
+++ client/src/components/UI/Modal/Modal.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import ReactModal from "react-modal";
+import PropTypes from "prop-types";
+import ButtonComponent from "../buttonComponent/ButtonComponent";
+
+import "./Modal.css";
+
+ReactModal.setAppElement("#root"); // cache l'arrière plan pour les lecteur d'écran
+
+function Modal({ isOpen, openFunc, contentLabel, Content, contentType }) {
+  const [confirmIsOpen, setConfirmIsOpen] = useState(false);
+
+  const closeModal = () => {
+    openFunc(false);
+    setConfirmIsOpen(false);
+  };
+
+  const handleCloseRequest = () => {
+    if (contentType === "form") {
+      setConfirmIsOpen(true);
+    } else {
+      closeModal();
+    }
+  };
+
+  const closeConfirmModal = () => {
+    setConfirmIsOpen(false);
+  };
+
+  return (
+    <>
+      <ReactModal
+        isOpen={isOpen}
+        contentLabel={contentLabel}
+        onRequestClose={handleCloseRequest}
+        shouldCloseOnEsc
+        shouldCloseOnOverlayClick
+        shouldFocusAfterRender
+        className="modal"
+        overlayClassName="modal-background"
+      >
+        <ButtonComponent
+          text="X"
+          handleClick={handleCloseRequest}
+          css="close-modal"
+        />
+        <Content />
+      </ReactModal>
+      <ReactModal
+        isOpen={confirmIsOpen}
+        contentLabel="confirmation de fermeture"
+        onRequestClose={closeConfirmModal}
+        shouldCloseOnEsc
+        shouldCloseOnOverlayClick
+        shouldFocusAfterRender
+        className="close-alert"
+        overlayClassName="modal-background"
+      >
+        <p className="paragraph-style">
+          Voulez-vous vraiment fermer sans sauvegarder ?
+        </p>
+        <div className="alert-btns">
+          <ButtonComponent
+            text="Oui"
+            handleClick={closeModal}
+            css="alert-btn"
+          />
+          <ButtonComponent
+            text="Non"
+            handleClick={closeConfirmModal}
+            css="alert-btn"
+          />
+        </div>
+      </ReactModal>
+    </>
+  );
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  openFunc: PropTypes.func.isRequired,
+  contentLabel: PropTypes.string.isRequired,
+  Content: PropTypes.elementType.isRequired,
+  contentType: PropTypes.string.isRequired,
+};
+
+export default Modal;
