0f29fd44a4083feb092a95a7126ffc553b4d355a
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-08-07 15:43:33 +0200

    Mise en place de la logique permettant d'ajouter un utilisateur avec confirmation de mot de passe

--- client/src/components/frontOffice/Forms/FormSignUp/FormSignUp.jsx
+++ client/src/components/frontOffice/Forms/FormSignUp/FormSignUp.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import InputComponent from "../../../UI/Form/inputComponent/InputComponent";
 import ButtonComponent from "../../../UI/buttonComponent/ButtonComponent";
 
+import connexion from "../../../../services/connexion";
 import "./FormSignUp.css";
 
 const initialUser = {
@@ -11,6 +12,7 @@ const initialUser = {
 
 function FormSignUp() {
   const [user, setUser] = useState(initialUser);
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleUserCreate = (event) => {
     const { name, value } = event.target;
@@ -19,12 +21,34 @@ function FormSignUp() {
       [name]: value,
     }));
   };
+  const handleConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+  const handleSubmitCreateUser = async (e) => {
+    e.preventDefault();
+    if (user.password === confirmPassword) {
+      try {
+        await connexion.post("/api/users/", user);
+      } catch (error) {
+        console.error("There was an error updating the profile!", error);
+      }
+      setUser((prev) => ({
+        ...prev,
+      }));
+      setUser(initialUser);
+      setConfirmPassword("");
+    } else {
+      setUser(initialUser);
+      setConfirmPassword("");
+    }
+  };
 
   return (
-    <form className="form-sign-up">
+    <form className="form-sign-up" onSubmit={handleSubmitCreateUser}>
       <InputComponent
         label="Email :"
         inputName="mail"
+        inputType="text"
         id="mail"
         inputValue={user.mail}
         handleChange={handleUserCreate}
@@ -32,11 +56,20 @@ function FormSignUp() {
       <InputComponent
         label="Mot de passe :"
         id="mail"
+        inputType="password"
         inputName="password"
         inputValue={user.password}
         handleChange={handleUserCreate}
       />
-      <InputComponent label="Confirmez votre mot de passe :" />
+      <InputComponent
+        inputType="password"
+        label="Confirmez votre mot de passe :"
+        inputName="confirm-password"
+        id="confirm-password"
+        inputValue={confirmPassword}
+        handleChange={handleConfirmPassword}
+      />
+      {user.password === confirmPassword ? "✅" : "❌"}
       <ButtonComponent text="Valider" css="button-submit" />
     </form>
   );
