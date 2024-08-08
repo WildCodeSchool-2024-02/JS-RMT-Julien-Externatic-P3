5e3adb9db4844ece48bda523ded1e0ced4fe669a
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-08-08 11:12:42 +0200

    nav en responsiv

--- client/src/components/frontOffice/Navigation/FrontNav.jsx
+++ client/src/components/frontOffice/Navigation/FrontNav.jsx
@@ -1,37 +1,62 @@
 import { Link } from "react-router-dom";
 
-import logoExternaticNav from "../../../assets/images/ExternaticLogoBlanc.png";
-import logoAvatar from "../../../assets/icones/user-white.svg";
-import logoSquare from "../../../assets/icones/square-white.svg";
+import logoExternaticNavWhite from "../../../assets/images/ExternaticLogoBlanc.png";
+import logoExternaticNavBlack from "../../../assets/images/ExternaticLogoNoir.png";
+import logoAvatarWhite from "../../../assets/icones/user-white.svg";
+import logoAvatarBlack from "../../../assets/icones/user-black.svg";
+import logoSquareWhite from "../../../assets/icones/square-white.svg";
+import logoSquareBlack from "../../../assets/icones/square-black.svg";
 
 import "./FrontNav.css";
 
 function FrontNav() {
   return (
-    <nav className="nav-front-container">
-      <ul className="nav-front-components">
-        <li className="nav-square">
-          <img src={logoSquare} alt="logo carré" />
-          <img src={logoSquare} alt="logo carré" />
-          <img src={logoSquare} alt="logo carré" />
-          <img src={logoSquare} alt="logo carré" />
-        </li>
-        <li>
-          <Link to="/">
-            <img
-              className="logo-externatic"
-              src={logoExternaticNav}
-              alt="logo externatic"
-            />
-          </Link>
-        </li>
-        <li>
-          <Link to="/candidat/:id">
-            <img className="logo-avatar" src={logoAvatar} alt="logo avatar" />
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="nav-front-container">
+        <ul className="nav-front-components">
+          <li className="nav-square-white">
+            <img src={logoSquareWhite} alt="logo carré" />
+            <img src={logoSquareWhite} alt="logo carré" />
+            <img src={logoSquareWhite} alt="logo carré" />
+            <img src={logoSquareWhite} alt="logo carré" />
+          </li>
+          <li className="nav-square-black">
+            <img src={logoSquareBlack} alt="logo carré" />
+            <img src={logoSquareBlack} alt="logo carré" />
+            <img src={logoSquareBlack} alt="logo carré" />
+            <img src={logoSquareBlack} alt="logo carré" />
+          </li>
+          <li>
+            <Link to="/">
+              <img
+                className="logo-externatic-white"
+                src={logoExternaticNavWhite}
+                alt="logo externatic"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="/candidat/:id">
+              <img
+                className="logo-avatar-white"
+                src={logoAvatarWhite}
+                alt="logo avatar"
+              />
+              <img
+                className="logo-avatar-black"
+                src={logoAvatarBlack}
+                alt="logo avatar"
+              />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <img
+        className="logo-externatic-black"
+        src={logoExternaticNavBlack}
+        alt="logo externatic"
+      />
+    </>
   );
 }
 
