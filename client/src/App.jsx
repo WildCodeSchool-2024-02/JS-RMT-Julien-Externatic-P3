969edbd21685802caf35665213156d3aa74cb91a
BastienDmr <domer.bastien@gmail.com>
2024-08-08 14:18:05 +0200

    Mise en place du template pour la page d'acceuil

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,100 @@
+import acceuilExt from "./assets/images/externatic-acceuil.jpg";
+import userRose from "./assets/icones/users-rose.svg";
+
 import "./App.css";
+import "./Home.css";
 
 function App() {
   return (
     <main>
       <h1>Externatic</h1>
+      <article className="container-presentation">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          fugiat sit eaque eius voluptatem minus voluptas, hic omnis ipsum
+          adipisci earum repellendus quia iure atque laudantium rem aliquam
+          reiciendis error. Est nihil, assumenda quis exercitationem tenetur
+          impedit perspiciatis recusandae nulla libero laborum.
+          <br />
+          Nulla exercitationem reprehenderit saepe sit veritatis ex minima porro
+          totam illum tenetur! Ipsa ipsam explicabo distinctio nisi libero?
+          Laudantium ducimus omnis ut cumque accusantium molestias quisquam,
+          minima inventore, veniam fugit iste eaque! In quo minima accusamus?
+          Eveniet, tempore repellendus quaerat tempora natus non nostrum hic
+          dolores.
+        </p>
+        <img src={acceuilExt} alt="acceuil 1" />
+      </article>
+      <h2>Nos offres du moments</h2>
+      <article className="container-presentation">
+        <img src={acceuilExt} alt="acceuil 2" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          fugiat sit eaque eius voluptatem minus voluptas, hic omnis ipsum
+          adipisci earum repellendus quia iure atque laudantium rem aliquam
+          reiciendis error. Est nihil, assumenda quis exercitationem tenetur
+          impedit perspiciatis recusandae nulla libero laborum.
+          <br /> Nulla exercitationem reprehenderit saepe sit veritatis ex
+          minima porro totam illum tenetur! Ipsa ipsam explicabo distinctio nisi
+          libero? Laudantium ducimus omnis ut cumque accusantium molestias
+          quisquam, minima inventore, veniam fugit iste eaque! In quo minima
+          accusamus? Eveniet, tempore repellendus quaerat tempora natus non
+          nostrum hic dolores. Temporibus, optio! Autem dignissimos commodi
+          provident dolorum incidunt perspiciatis nesciunt distinctio ullam,
+          debitis dicta
+        </p>
+      </article>
+      <section className="container-value">
+        <div>
+          <p className="number-value">40</p>
+          <p>lorem lorem</p>
+        </div>
+        <div>
+          <p className="number-value">+ 400</p>
+          <p>lorem lorem</p>
+        </div>
+        <div>
+          <p className="number-value">85%</p>
+          <p>lorem lorem</p>
+        </div>
+        <div>
+          <p className="number-value">+ 4000</p>
+          <p>lorem lorem</p>
+        </div>
+      </section>
+      <section className="container-acceuil-consultant">
+        <article>
+          <img src={userRose} alt="consultant" />
+          <h3>lorem lorem</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam odio
+            ab animi eos modi eum itaque sapiente incidunt eaque beatae
+            veritatis hic illum repellat nobis consequatur aliquid, sequi
+            quaerat mollitia?
+          </p>
+        </article>
+        <article>
+          <img src={userRose} alt="consultante" />
+          <h3>lorem lorem</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+            sapiente voluptates inventore unde sunt quidem incidunt
+            consequuntur, delectus deleniti ut repellat perferendis at rerum
+            maxime ullam ipsam illo dolores quaerat!
+          </p>
+        </article>
+        <article>
+          <img src={userRose} alt="consultant 2" />
+          <h3>lorem lorem</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+            sapiente voluptates inventore unde sunt quidem incidunt
+            consequuntur, delectus deleniti ut repellat perferendis at rerum
+            maxime ullam ipsam illo dolores quaerat!
+          </p>
+        </article>
+      </section>
+      <h2>Nos entreprises partenaires</h2>
     </main>
   );
 }
