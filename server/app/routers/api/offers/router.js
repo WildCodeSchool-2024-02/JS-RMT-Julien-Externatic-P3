8dbfeab5df65f71461391e6e8338f3c9ece4e9f3
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-08-19 15:10:50 +0200

    browse offer backend

--- server/app/routers/api/offers/router.js
+++ server/app/routers/api/offers/router.js
@@ -5,14 +5,12 @@ const router = express.Router();
 const {
   browse,
   read,
-  browseByConsultant,
   add,
 } = require("../../../controllers/offerActions");
 
 const validateOffer = require("../../../services/validateOffer");
 
 router.get("/", browse);
-router.get("/consultant", browseByConsultant);
 router.get("/:id", read);
 router.post("/", validateOffer, add);
 
