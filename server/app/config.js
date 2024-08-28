f3f3719c1a25ca79da3834e9b57c56478e333c7b
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-08-28 17:09:36 +0200

    debug

--- server/app/config.js
+++ server/app/config.js
@@ -1,7 +1,7 @@
 // Load the express module to create a web application
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
