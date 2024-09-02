e51423fd63a2d0af31437363e73a8bf19bdd60a8
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-09-02 18:09:55 +0200

    Correction orthographe navbar

--- client/src/components/frontOffice/Navigation/FrontNav.jsx
+++ client/src/components/frontOffice/Navigation/FrontNav.jsx
@@ -105,7 +105,7 @@ function FrontNav() {
               {isDropdownExploreOpen && (
                 <div className="dropdown-explore-menu">
                   <Link to="/" className="dropdown-link">
-                    Acceuil
+                    Accueil
                   </Link>
                   <Link to="/offres" className="dropdown-link">
                     Toutes nos offres
