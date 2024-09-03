23c3229b75b93557b081ce23404436c02942c148
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-09-03 15:41:01 +0200

    sauvegarde user dans le context

--- client/src/context/ExternaticContext.jsx
+++ client/src/context/ExternaticContext.jsx
@@ -1,4 +1,11 @@
-import { createContext, useState, useMemo, useContext } from "react";
+import {
+  createContext,
+  useState,
+  useMemo,
+  useContext,
+  useEffect,
+  useCallback,
+} from "react";
 import PropTypes from "prop-types";
 
 // Créer le contexte utilisateur
@@ -8,10 +15,28 @@ export function ExternaticProvider({ children }) {
   // État pour stocker les informations utilisateur
   const [logedUser, setLogedUser] = useState(null);
 
+  const handleUser = useCallback((user) => {
+    if (user) {
+      localStorage.setItem("LoggedUser", JSON.stringify(user));
+      setLogedUser(user);
+    } else {
+      localStorage.removeItem("LoggedUser");
+      setLogedUser(null);
+    }
+  }, []);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("LoggedUser");
+    if (savedUser) {
+      setLogedUser(JSON.parse(savedUser));
+    }
+    setLogedUser(null);
+  }, []);
+
   // Mémorisation de l'objet value pour éviter les rendus inutiles
   const value = useMemo(
-    () => ({ logedUser, setLogedUser }),
-    [logedUser, setLogedUser]
+    () => ({ logedUser, handleUser }),
+    [logedUser, handleUser]
   );
 
   return (
