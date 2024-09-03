46a4d43411a80c715bca5d2e783e2d28bfb1a855
Oresti <hoxhallarioresti02@gmail.com>
2024-09-03 10:43:43 +0200

    Resolution de conflis nom de fichier

--- server/database/tables.js
+++ server/database/tables.js
@@ -10,7 +10,7 @@ const WorkFormatRepository = require("./models/WorkFormatRepository");
 const CategoryRepository = require("./models/CategoryRepository");
 const StudyLevelRepository = require("./models/StudyLevelRepository");
 const TechnologyRepository = require("./models/TechnologyRepository");
-const CandidacyRepository = require("./models/CandidacyRepository_merge");
+const CandidacyRepository = require("./models/CandidacyRepository");
 const FavoriteRepository = require("./models/FavoriteRepository");
 
 // Create an empty object to hold data repositories for different tables
