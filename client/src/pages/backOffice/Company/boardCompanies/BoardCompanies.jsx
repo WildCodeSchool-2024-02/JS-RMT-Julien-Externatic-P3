c5fbb702bbb9dd71f8cb661d19542e2774c4320e
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-09-04 16:45:44 +0200

    Rajout de la barre de recherche pour les pages boards entreprises côté admin et consultant

--- client/src/pages/backOffice/Company/boardCompanies/BoardCompanies.jsx
+++ client/src/pages/backOffice/Company/boardCompanies/BoardCompanies.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 
 import BoardList from "../../../../components/backOffice/boardComponent/BoardList";
+import SearchBar from "../../../../components/UI/searchBar/SearchBar";
 
 import { useExternatic } from "../../../../context/ExternaticContext";
 
@@ -8,16 +9,33 @@ function BoardCompanies() {
   const { logedUser } = useExternatic();
   const companies = useLoaderData();
   return (
-    <BoardList
-      datas={companies}
-      pathFront={
-        logedUser.role_id === 3
-          ? "/admin/entreprises"
-          : "/consultants/entreprises"
-      }
-      pathBack=""
-      deleted={false}
-    />
+    <>
+      <section className="section-search">
+        <SearchBar
+          path={
+            logedUser.role_id === 3
+              ? "/admin/entreprises"
+              : "/consultants/entreprises"
+          }
+        />
+      </section>
+      {companies.length !== 0 ? (
+        <BoardList
+          datas={companies}
+          pathFront={
+            logedUser.role_id === 3
+              ? "/admin/entreprises"
+              : "/consultants/entreprises"
+          }
+          pathBack=""
+          deleted={false}
+        />
+      ) : (
+        <h2 className="no-result-search">
+          Aucun element ne correspond à votre recherche
+        </h2>
+      )}
+    </>
   );
 }
 
